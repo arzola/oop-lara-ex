6c9e02957a7697a3e1b356d4d59d01455b20382e
Oscar Arzola <oscararzola@gmail.com>
2017-10-13 15:03:50 -0500

    Update CleanFunctions.php

--- week3/CleanFunctions.php
+++ week3/CleanFunctions.php
@@ -1,9 +1,11 @@
 <?php
 //Bad
-function ($to, $from, $attachments, $cc, $bcc)
+function query($table, $fields, $wheres, $orderby)
 {
 
 }
+
+query('table_users','id,name,title',['name>4'],['title'=>'asc']);
 
 //Good
 //TODO
